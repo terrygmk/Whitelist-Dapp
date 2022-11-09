62d3f08677e227683d6aa29333a77ba8df4e9d8e
terrygmk <terrygmk@gmail.com>
2022-11-09 14:36:13 +0800

    fix whitelistedAddresses

--- hardhat-tutorial/contracts/Whitelist.sol
+++ hardhat-tutorial/contracts/Whitelist.sol
@@ -6,16 +6,16 @@ contract Whitelist {
     uint8 public maxWhitelistedAddresses ;
     uint8 public numAddressesWhitelisted ;
 
-    mapping(address => bool) public whitelistedAddress;
+    mapping(address => bool) public whitelistedAddresses;
 
     constructor(uint8 _maxWhitelistedAddresses) {
         maxWhitelistedAddresses = _maxWhitelistedAddresses;
     }
 
     function addAddressToWhitelist() public {
-        require(!whitelistedAddress[msg.sender], "Sender has already already whitelisted");
+        require(!whitelistedAddresses[msg.sender], "Sender has already already whitelisted");
         require(numAddressesWhitelisted < maxWhitelistedAddresses, "Max number of addresses already whitelisted");
-        whitelistedAddress[msg.sender] = true;
+        whitelistedAddresses[msg.sender] = true;
         numAddressesWhitelisted += 1;
     }
 }
--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1,66 +1,66 @@
-export const WHITELIST_CONTRACT_ADDRESS = "0x010cdc60b2ec9ccfb52feb4cef0825dfb170245f";
+export const WHITELIST_CONTRACT_ADDRESS = "0x9A6Eb6503Fd78B68a1FA7AfAa8fB13A4030BFcC8";
 export const abi = [
-    {
-      "inputs": [
-        {
-          "internalType": "uint8",
-          "name": "_maxWhitelistedAddresses",
-          "type": "uint8"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [],
-      "name": "addAddressToWhitelist",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "maxWhitelistedAddresses",
-      "outputs": [
-        {
-          "internalType": "uint8",
-          "name": "",
-          "type": "uint8"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "numAddressesWhitelisted",
-      "outputs": [
-        {
-          "internalType": "uint8",
-          "name": "",
-          "type": "uint8"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "name": "whitelistedAddress",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ];
+  {
+    "inputs": [
+      {
+        "internalType": "uint8",
+        "name": "_maxWhitelistedAddresses",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "addAddressToWhitelist",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "maxWhitelistedAddresses",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "numAddressesWhitelisted",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "whitelistedAddresses",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
